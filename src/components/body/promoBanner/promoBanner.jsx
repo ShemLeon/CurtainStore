c11b44dcf65e0300206eb1c34824e9fd7ae2f0a5
ShemLeon <shemiakinleonid@gmail.com>
2025-10-14 16:06:47 +0300

    promo

--- src/components/body/promoBanner/promoBanner.jsx
+++ src/components/body/promoBanner/promoBanner.jsx
@@ -2,13 +2,32 @@ import styles from "./promoBanner.module.css";
 
 function PromoBanner() {
   return (
-      <section className={styles.promoBanner}>
-        <div className={styles.promoContent}>
-          <h2>FREE SHIPPING ON ORDERS OVER $100</h2>
-          <p>Only Applicable Within The Continental United States. Not Applicable For Long Curtain Rods or for Shipments to the West Coast.</p>
-          <span className={styles.promoCode}>Use code: 100FREE</span>
+    <section className={styles.promoSection}>
+      <div className={styles.promoContainer}>
+        <div className={styles.promoLeft}>
+          <img src="/src/assets/images/categories/body/left_promo.jpg" alt="Interior Design" />
         </div>
-      </section>
+        
+        <div className={styles.promoCenter}>
+          <div className={styles.promoBanner}>
+            <h2 className={styles.promoTitle}>
+              FREE SHIPPING ON<br />
+              <span className={styles.promoHighlight}>ORDERS OVER $100</span>
+            </h2>
+            <p className={styles.promoText}>
+              Only Applicable Within The Continental United States. Not Applicable For Long<br />
+              Curtain Rods or for Shipments to the West Coast.
+            </p>
+            <span className={styles.promoCode}>Use code: <strong>100FREE</strong></span>
+            <button className={styles.shopButton}>SHOP CURTAINS</button>
+          </div>
+        </div>
+        
+        <div className={styles.promoRight}>
+          <img src="/src/assets/images/categories/body/right_promo.jpg" alt="Interior Design" />
+        </div>
+      </div>
+    </section>
   );
 }
 
