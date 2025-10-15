e3f34c29d0b1f3030d74d71de20acdee7ad772b5
ShemLeon <shemiakinleonid@gmail.com>
2025-10-15 19:48:06 +0300

    custom pinch block start

--- src/components/body/Body.jsx
+++ src/components/body/Body.jsx
@@ -1,11 +1,12 @@
 import styles from "./Body.module.css";
 import PromoBanner from "./promoBanner/promoBanner";
+import CustomPinch from "./customPinch/customPinch";
 
 function Body() {
   return (
   <main className={styles.body}>
     <PromoBanner />
-
+    <CustomPinch />
   </main>
   )
 }
---
+++ src/components/body/customPinch/customPinch.jsx
@@ -0,0 +1,26 @@
+import styles from "./customPinch.module.css";
+
+function CustomPinch() {
+  return (
+    <section className={styles.pinch}>
+      {/* Left side - Image */}
+      <div className={styles.pinchLeft}>
+        <img
+          src="/src/assets/images/categories/body/left_promo.jpg"
+          alt="Custom Pinch Pleated Sheers"
+        />
+     </div>
+
+     {/* Right side - Content */}
+      <div className={styles.pinchRight}>
+        <p className={styles.subtitle}>THE CURTAIN SHOP</p>
+        <h2>Custom Pinch Pleated Sheers</h2>
+        <button className={styles.exploreButton}>EXPLORE</button>
+      </div>
+
+
+    </section>
+  );
+}
+
+export default CustomPinch;
