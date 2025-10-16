d5ba4bf826f8020ac87538895235a280c7450cdc
ShemLeon <shemiakinleonid@gmail.com>
2025-10-16 23:19:57 +0300

    Sale body

--- src/components/body/Body.jsx
+++ src/components/body/Body.jsx
@@ -1,12 +1,15 @@
 import styles from "./Body.module.css";
 import PromoBanner from "./promoBanner/promoBanner";
 import CustomPinch from "./customPinch/customPinch";
+import Sale from "./sale/sale";
 
 function Body() {
   return (
   <main className={styles.body}>
     <PromoBanner />
     <CustomPinch />
+    <Sale />
+
   </main>
   )
 }
