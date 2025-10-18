32fccdb2adfcc0ca94fb3a694e03cce69cfece1b
ShemLeon <shemiakinleonid@gmail.com>
2025-10-19 00:20:42 +0300

    fot

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,18 @@
+import styles from "./Body.module.css";
+import PromoBanner from "./promoBanner/promoBanner";
+import CustomPinch from "./customPinch/customPinch";
+import Sale from "./sale/sale";
+import ShopByCategory from "./shopByCategory/shopByCategory";
+
+function Footer() {
+  return (
+    <main className={styles.footer}>
+         < />
+      <Copyrigth />
+
+     
+    </main>
+  );
+}
+
+export default Footer;
